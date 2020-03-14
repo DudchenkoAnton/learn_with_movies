a228bcf98954c9b13d5ea35902c9eba643170c56
yael <yael4391@gmail.com>
2020-03-14 16:50:09 +0200

    add image and searce in the create lesson screen

--- lib/components/lesson_card.dart
+++ lib/components/lesson_card.dart
@@ -1,9 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:temp_project/database/lesson_db.dart';
+
 import 'package:temp_project/utilites/lesson_objects.dart';
 // this class is how the card will look like
 
 class card_movie extends StatefulWidget {
-  LessonData videoObject;
+  LessonDB videoObject;
   Function delete;
   Function edit;
 
@@ -18,61 +21,98 @@ class card_movie extends StatefulWidget {
 }
 
 class _card_movieState extends State<card_movie> {
-  String calc_time(int start, int end) {
-    int res = end - start;
+  String calc_time(double start, double end) {
+    double res = (end - start)/60;
     return res.toString();
   }
+/*
+  String thumb = await Thumbnails.getThumbnail(
+  thumbnailFolder:'[FOLDER PATH TO STORE THUMBNAILS]', // creates the specified path if it doesnt exist
+  videoFile: '[VIDEO PATH HERE]',
+  imageType: ThumbFormat.PNG,
+  quality: 30);
+*/
+
+   String url_image(youtubeUrl){
+    Uri uri = Uri.parse(youtubeUrl);
+    String videoID=uri.queryParameters["v"];
+    String url = "http://img.youtube.com/vi/" + videoID +"/0.jpg";
+    return url;
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Card(
+      color: Colors.grey[200],
       margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
       child: Padding(
         padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              widget.videoObject.name,
-              style: TextStyle(fontSize: 20.0, color: Colors.grey),
-            ),
-            SizedBox(
-              height: 6.0,
-            ),
-            Text(
-              widget.videoObject.url,
-              style: TextStyle(fontSize: 18.0, color: Colors.grey),
-            ),
-            SizedBox(
-              height: 6.0,
-            ),
-            Text(
-              calc_time(widget.videoObject.start.inSeconds,
-                  widget.videoObject.end.inMilliseconds),
-              style: TextStyle(fontSize: 18.0, color: Colors.grey),
-            ),
-            Row(
-              children: <Widget>[
-                SizedBox(
-                  width: 150.0,
-                ),
-                FlatButton.icon(
-                  onPressed: this.widget.edit,
-                  label: Text('Edit'),
-                  icon: Icon(Icons.edit),
-                ),
-                SizedBox(
-                  width: 10.0,
-                ),
-                FlatButton.icon(
-                  onPressed: this.widget.delete,
-                  label: Text('Delete'),
-                  icon: Icon(Icons.delete),
-                ),
+        child:
+            Row(children: <Widget>[
+              Container(
+              height: 90.0,
+              width: 100.0,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image:NetworkImage(url_image(widget.videoObject.getMainVideoURL())), fit: BoxFit.cover),
+              ),
+              ),
+              SizedBox(
+                width: 20.0,
+              ),
+              Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Column(
+                        //crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          Text(
+                            widget.videoObject.getMainVideoName(),
+                            style: TextStyle(fontSize: 20.0, color: Colors.black),
+                            textAlign: TextAlign.start,
+                          ),
+                          SizedBox(
+                            height: 8.0,
+                          ),
+                          Text(
+                            calc_time(widget.videoObject.getMainVideoStartTime(),
+                                widget.videoObject.getMainVideoEndTime())+' min',
+                            style: TextStyle(fontSize: 15.0, color: Colors.black),
+                            textAlign: TextAlign.start,
+                          ),
+                ],
+              )
+            ],
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      SizedBox(
+                        width: 110.0,
+                      ),
+                      IconButton(
+                        onPressed: this.widget.edit,
+                        icon: Icon(Icons.edit),
+                      ),
+                      SizedBox(
+                        width: 2.0,
+                      ),
+                      IconButton(
+                        onPressed: this.widget.delete,
+                        icon: Icon(Icons.delete),
+                      ),
+                    ],
+                  )
+
+
+            ]
+              ),
               ],
             ),
-          ],
-        ),
       ),
     );
   }
--- lib/screens/lessons_list_screen.dart
+++ lib/screens/lessons_list_screen.dart
@@ -2,42 +2,68 @@ import 'package:flutter/material.dart';
 import 'package:temp_project/utilites/lesson_objects.dart';
 import 'package:temp_project/utilites/lesson_objects.dart';
 import 'package:temp_project/components/lesson_card.dart';
+import 'package:temp_project/database/lesson_db.dart';
+import 'package:temp_project/database/database_utilities.dart';
+
 import 'package:temp_project/screens/video_creator_screen.dart';
 
-class LessonsListScreen extends StatefulWidget {
+class LessonsListScreen  extends StatefulWidget{
   static const String id = 'lessons_list_screen';
-  List<LessonData> card = [];
-  @override
-  _LessonsListScreenState createState() => _LessonsListScreenState();
+
+   @override
+  _LessonsListScreenState  createState()=> _LessonsListScreenState ();
+
 }
+class _LessonsListScreenState  extends State<LessonsListScreen>{
+  //the search of a word
+  DatabaseUtilities db;
+  var _searceView=new TextEditingController();
+  List<LessonDB> all_lesson;
 
-class _LessonsListScreenState extends State<LessonsListScreen> {
-  //list of all the cards
+
 
   @override
-  Widget build(BuildContext context) {
-    Widget cardTemplate(videoObject, delete, edit) {
-      return new card_movie(videoObject, delete, edit);
-    }
+  void initState(){
+    super.initState();
+    all_lesson.addAll(db.getLessonsFromDB());
+  }
 
+  void filterSearchResults(String query) {
+    if (query.isNotEmpty) {
+      List<LessonDB> dummyListData = List<LessonDB>();
+      all_lesson.forEach((item) {
+        if (item.getMainVideoName().contains(query)) {
+          dummyListData.add(item);
+        }
+      });
+      setState(() {
+        all_lesson.clear();
+        all_lesson.addAll(dummyListData);
+      });
+      return;
+    } else {
+      setState(() {
+        all_lesson.clear();
+        all_lesson.addAll(db.getLessonsFromDB());
+      });
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Lesson'),
-        centerTitle: true,
+      appBar: new AppBar(
+        title: new Text("Create Lesson with movies"),
       ),
-      body: Column(
-        //show all the card in the list of card
-        children: widget.card
-            .map((Video) => cardTemplate(Video, () {
-                  setState(() {
-                    widget.card.remove(Video);
-                  });
-                }, () {
-                  setState(() {
-                    edit_card(context, Video);
-                  });
-                }))
-            .toList(),
+      body: Container(
+         margin: EdgeInsets.only(left: 10.0,right: 10.0,top: 10.0),
+         child:Column(
+           children: <Widget>[
+             _createSearchView(),
+             _CardView(),
+           ],
+         )
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -49,26 +75,66 @@ class _LessonsListScreenState extends State<LessonsListScreen> {
     );
   }
 
-  void edit_card(BuildContext context, videoObject) async {
-    videoObject = await Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (context) => VideoCreatorScreen(
-                  videoData: videoObject,
-                )));
-    setState(() {
-      videoObject;
-    });
+
+
+
+
+  Widget _CardView(){
+    return new Column(
+      //show all the card in the list of card
+      children: all_lesson.map((lesson_object) => new card_movie(lesson_object, () {
+        setState(() {
+          all_lesson.remove(lesson_object);
+        });
+      }, () {
+        setState(() {
+          edit_card(context, lesson_object);
+        });
+      }))
+          .toList(),
+    );
   }
 
+
+
+  void edit_card(BuildContext context, lesson_object) async {
+    lesson_object = await Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) => VideoCreatorScreen(videoData: lesson_object,)
+        ));
+    setState(() {
+      lesson_object;
+    });
+  }
+  Widget _createSearchView(){
+    return new Container(
+        child:
+        TextField(
+          onChanged: (value) {
+            filterSearchResults(value);
+          },
+          controller: _searceView,
+          decoration: InputDecoration(
+              labelText: "Search",
+              hintText: "Search",
+              prefixIcon: Icon(Icons.search),
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(25.0)))),
+     ),
+    );
+  }
+
+
   void add_card(BuildContext context) async {
-    final video_new = await Navigator.push(
+    final lesson_new = await Navigator.push(
         context, MaterialPageRoute(builder: (context) => VideoCreatorScreen()));
-    if (video_new != null) {
+    if (lesson_new != null) {
       setState(() {
-        ////here we need to call your screen and the data you return should be CardObject object
-        widget.card.add(video_new);
+        //lesson_new need to by from the shape of LessonDB
+        all_lesson.add(lesson_new);
       });
     }
   }
 }
+
