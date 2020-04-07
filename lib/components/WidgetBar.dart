abe14594ecaeba8a2ab5f47c8e6e6d6832e25c7e
yael <yael4391@gmail.com>
2020-04-07 19:02:38 +0300

    user screen and log in screen

---
+++ lib/components/WidgetBar.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:temp_project/database/lesson_db.dart';
+import 'package:temp_project/database/database_utilities.dart';
+import 'package:temp_project/screens/video_creator_screen.dart';
+
+
+
+
+class WidgetBar extends StatefulWidget {
+  final String show;
+  WidgetBar({Key key, this.show}) : super(key: key);
+
+  @override
+  _WidgetBarState createState() => _WidgetBarState();
+
+}
+class _WidgetBarState extends State<WidgetBar>{
+  DatabaseUtilities db = new DatabaseUtilities();
+  var _searchView = new TextEditingController();
+  List<LessonDB> allLesson = List<LessonDB>();
+  var animationOn = true;
+  Icon cusIcon=Icon(Icons.search);
+  Widget cusSearchBar=Text("Learn With Movies");
+
+
+
+  @override
+  void initState() {
+    _getThingsOnStartup(this.widget.show).then((value) {});
+    super.initState();
+  }
+
+  Future _getThingsOnStartup(show) async {
+    List<LessonDB> list = await db.getLessonsFromDB();
+    allLesson.clear();
+    /////////sort by show
+    allLesson.addAll(list);
+    animationOn = false;
+    setState(() {
+      build(context);
+    });
+  }
+
+  void filterSearchResults(String query) {
+    if (query.isNotEmpty) {
+      List<LessonDB> dummyListData = List<LessonDB>();
+      allLesson.forEach((item) {
+        if (item.getLessonName().contains(query)) {
+          dummyListData.add(item);
+        }
+      });
+      setState(() {
+        allLesson.clear();
+        allLesson.addAll(dummyListData);
+      });
+      return;
+    } else {
+      setState(() async {
+        _getThingsOnStartup(this.widget.show).then((value) {});
+      });
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+  return Scaffold(
+    appBar:  AppBar(
+      leading: IconButton(
+        onPressed: (){
+          setState((){
+            searchAction();
+          });},
+        icon: cusIcon,
+      ),
+      centerTitle: true,
+      title: cusSearchBar,
+    ),
+    body: animationOn ? create_animation() : _CardView(context),
+  );}
+
+
+  void searchAction(){
+    if(this.cusIcon.icon==Icons.search){
+      this.cusIcon=Icon(Icons.clear);
+      this.cusSearchBar=TextField(
+        onChanged: (value) {
+          filterSearchResults(value);
+        },
+        controller: _searchView,
+
+        textInputAction: TextInputAction.go,
+        decoration: InputDecoration(
+            hintText: "Search Movie",
+            hintStyle: TextStyle(
+              color: Colors.black26,
+            ),
+            border: UnderlineInputBorder(),),
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 16.0,
+        ),
+      );
+    }
+    else{
+      this.cusIcon=Icon(Icons.search);
+      this.cusSearchBar=Text("Learn With Movies");
+    }
+  }
+
+  Widget _CardView(context){
+   return ListView.separated(
+       itemBuilder: (context,index){
+         return Column(
+           children: <Widget>[
+             SizedBox(height: 4),
+         GestureDetector(
+         onTap:(){move_screen(context); },
+          child: Container(
+               width: MediaQuery.of(context).size.width-10.0,
+               height: 200.0,
+               decoration: BoxDecoration(
+                 image: DecorationImage(
+                     image: NetworkImage(url_image(allLesson[index].videoURL))
+                     ,fit: BoxFit.cover)
+               ),
+             ),),
+             ListTile(
+               title:Text(allLesson[index].lessonName),
+               subtitle: Text("${allLesson[index].getVideoLenght()} min"),
+             ),
+           ],
+         );
+       },
+       separatorBuilder: (context,index)=>Divider(
+         height: 1.0,
+         color: Colors.grey,
+       ),
+       itemCount: allLesson.length);
+  }
+  void move_screen(BuildContext context) async {
+    ///change the name of the screen and send the lesson!
+    await Navigator.push(context, MaterialPageRoute(builder: (context) => VideoCreatorScreen()));
+  }
+
+  String url_image(youtubeUrl) {
+    Uri uri = Uri.parse(youtubeUrl);
+    String videoID = uri.queryParameters["v"];
+    String url = "http://img.youtube.com/vi/" + videoID + "/0.jpg";
+    return url;
+  }
+
+  Widget create_animation() {
+    return Container(
+      color: Colors.grey[50],
+      width: 300.0,
+      height: 300.0,
+      child: SpinKitFadingCircle(
+        itemBuilder: (_, int index) {
+          return DecoratedBox(
+            decoration: BoxDecoration(
+              color: index.isEven ? Colors.red : Colors.green,
+            ),
+          );
+        },
+        size: 120.0,
+      ),
+    );
+  }
+
+
+}
---
+++ lib/screens/UserChooseLesson.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:temp_project/components/WidgetBar.dart';
+class UserChooseLesson extends StatefulWidget {
+  static const String id = 'lessons_list_screen';
+
+  @override
+  _UserChooseLessonState createState() => _UserChooseLessonState();
+}
+
+class _UserChooseLessonState extends State<UserChooseLesson> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    WidgetBar(show: "home",),
+    WidgetBar(show: "Best Movies",),
+    WidgetBar(show: "History"),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _children[_currentIndex], // new
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped, // new
+        currentIndex: _currentIndex, // new
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            title: new Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.star_border),
+            title: new Text('Best Movies'),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.bookmark_border),
+              title: Text('History')
+          )
+        ],
+      ),
+    );
+  }
+
+}
