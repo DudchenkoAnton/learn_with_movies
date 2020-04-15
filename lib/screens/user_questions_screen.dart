fde3c2c93da2dbc7424f5663272e773c97c2f6ff
Anton <didchenkoanton94@gmail.com>
2020-04-15 13:14:46 +0300

    Merged video and question screens

--- lib/screens/user_questions_screen.dart
+++ lib/screens/user_questions_screen.dart
@@ -12,15 +12,6 @@ class UserQuestionsScreen extends StatefulWidget {
   UserQuestionsScreen({Key key, this.title, @required this.lessonDB})
       : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -88,7 +79,6 @@ class _UserQuestionsScreenState extends State<UserQuestionsScreen> {
     lesson = widget.lessonDB;
     questions = lesson.getQuestionsList();
 
-
     QuestionDB question = questions[0];
 
     curQuestionStartPoint = question.getVideoStartTime();
@@ -123,16 +113,9 @@ class _UserQuestionsScreenState extends State<UserQuestionsScreen> {
   @override
   Widget build(BuildContext context) {
     question = lesson.getQuestionsList()[_cur_question - 1];
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
         actions: <Widget>[
           RawMaterialButton(
