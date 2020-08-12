232e9f6aa0d38b4c07601b8c8303e6d04ec4a713
Anton <didchenkoanton94@gmail.com>
2020-08-12 20:10:59 +0300

    Changed "about us" text.

--- lib/screens/about_us_screen.dart
+++ lib/screens/about_us_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:temp_project/utilites/texts.dart';
 
 class AboutUs extends StatelessWidget {
   @override
@@ -14,15 +15,26 @@ class AboutUs extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-            Padding(padding:EdgeInsets.all(16.0),child:Text("Thank you for choosing to learn with us! We are a group of three students and created this app as a part of our final project of our degree. The purpose of the app is to create a pleasant and sufficient way to learn English.",
-            style: TextStyle(fontSize: 30.0,fontWeight: FontWeight.bold),),),
-            Center(child:Text("Problem or Question ? – let us know. Reach us at: learnwith@gmail.com",
-              style: TextStyle(fontSize: 25.0),textAlign: TextAlign.center,
-            ),),
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                tAboutUsMain,
+                style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(4.0),
+              child: Center(
+                child: Text(
+                  tAboutUsSecondary,
+                  style: TextStyle(fontSize: 25.0),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
           ],
         ),
       ),
-
     );
   }
 }
--- lib/utilites/texts.dart
+++ lib/utilites/texts.dart
@@ -1,2 +1,5 @@
 String tInitialLessonText =
     'Now you gonna see some video. Listen carefully, cause after that you will answer questions about what you heard in video. Good Luck!';
+String tAboutUsMain =
+    'This app was created to improve the level of English in a fun way and out of the content worlds close to each and every one.';
+String tAboutUsSecondary = 'Question or idea? We would love to know: Learnwith@gmail.com';
